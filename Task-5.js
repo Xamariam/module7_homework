3cfcc071959d1328d9123b3404ab277138b25fe4
Egor <96489205+Xamariam@users.noreply.github.com>
2023-04-15 15:32:41 +0700

    Update Task-5.js

--- Task-5.js
+++ Task-5.js
@@ -1,33 +1,85 @@
 // Задание 5
 
-class ElectricalAppliance
-{
-  constructor(name, power)
-  {
-    this.name = name;
-    this.power = power;
-    this.isPlugged = false;
+class Parent{
+  
+  constructor (name,color, brand, wattage){
+    
+  this.poweredBy = "electricity";
+  this.name = name;
+  this.color = color;
+  this.brand = brand;
+  this.wattage = wattage;
+  this.isPlugged = false; //по умолчанию всегда не включены в розетку
   }
   
-  plugIn()
-  {
-    console.log(this.name + " is plugged!");
-    this.isPlugged = true;
+  plugIn(){
+    
+  if(this.isPlugged !== true){
+  console.log(`${this.name} is plugged in!`);
+  this.isPlugged = true;
   }
+    
+  else {
+    console.log(`${this.name} is already plugged!`);
+}
+}
   
-  getPowerUsed()
-  {
-    return this.isPlugged ? this.power : 0;
+  unplug(){
+    
+  if(this.isPlugged !== false){
+  console.log(`${this.name} is unplugged!`);
+  this.isPlugged = false;
+  }
+  else {
+    console.log(`${this.name} already unplugged!`);
+  }
+}
+  
+}
+
+//тут я оптимизировал код, до этого отдельно для каждого прописывал this.brand и 
+// this.wattage, сейчас убрал всё это в родителя.
+
+class Lamp extends Parent{
+  colorTemp;
+  constructor(name, color, brand, wattage, colorTemp) {
+    super(name, color, brand, wattage);
+    this.colorTemp = colorTemp;
   }
 }
 
-const lamp = new ElectricalAppliance('lampa', 45);
-const tv = new ElectricalAppliance('tv', 1000);
-
-console.log(lamp.getPowerUsed() + tv.getPowerUsed());
-
-lamp.plugIn();
-console.log(lamp.getPowerUsed() + tv.getPowerUsed());
-
-tv.plugIn();
-console.log(lamp.getPowerUsed() + tv.getPowerUsed());
+  class Computer extends Parent {
+    functionality;
+    constructor(name, color, brand, wattage, functionality) {
+      super(name, color, brand, wattage);
+      this.functionality = functionality; //for work||for home||for gaming
+    }
+  }
+  
+  class Heater extends Parent {
+    type;
+    constructor(name, color, brand, wattage, type) {
+      super(name, color, brand, wattage);
+      this.type = type; // oil||air||infrared
+    }
+  }
+    
+  
+  // экземпляр лампы
+  const tableLamp = new Lamp ("Desktop Lamp", "white", "Xiaomi", 50, 3500);
+  // экземпляр компьютера
+  const homePC = new Computer("Personal Computer", "black", "Intel", 900, "for work");
+  //экземпляр обогревателя
+  const oilHeater = new Heater ("Oil Filled Radiator", "grey", "Orient", 700, "oil");
+  // отключить обогреватель из розетки
+  oilHeater.unplug(); //пробуем выключить - не получается, т.к. уже выключен
+  oilHeater.plugIn();//включаем в розетку
+  oilHeater.plugIn()//пробуем снова включить - не выходит
+  // включить компьютер в розетку
+  homePC.plugIn();
+  //отключить лампу
+  tableLamp.plugIn();
+  // результат
+  console.log(homePC)
+  console.log(tableLamp)
+  console.log(oilHeater)
